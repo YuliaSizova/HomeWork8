1d80cb9c3fb2b6917d4cdc187deb20b358572611
ilia <ilia100@hotmail.com>
2023-02-12 19:03:40 +0300

    Task60,62

---
+++ Task60/Program.cs
@@ -0,0 +1,106 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)*/
+
+
+
+Dictionary<int, int> GetDictionery(int[,,] matrix)
+{
+    Dictionary<int, int> Slovar = new Dictionary<int, int>();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                if (Slovar.ContainsKey(matrix[i, j, z]))
+                {
+                    Slovar[matrix[i, j, z]] = Slovar[matrix[i, j, z]] + 1;
+                }
+                else
+                {
+                    Slovar.Add(matrix[i, j, z], 1);
+                }
+            }
+        }
+
+    }
+    return Slovar;
+}
+
+bool GetCheker(int[,,] matrix, Dictionary<int, int> Slovar)
+{
+    bool temp = true;
+    foreach (var item in Slovar)
+    {
+        if (item.Value > 1)
+        {
+            temp = false;
+        }
+    }
+    return temp;
+}
+
+
+
+
+int[,,] Generate3DArray(int m, int n, int o)
+{
+
+    int[,,] result = new int[m, n, o];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int z = 0; z < result.GetLength(2); z++)
+
+            {
+                result[i, j, z] = new Random().Next(1, 100);
+            }
+        }
+
+    }
+    return result;
+}
+void Print3DArray(int[,,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int z = 0; z < arr.GetLength(2); z++)
+
+            {
+                if (arr[i, j, z] < 10) Console.Write($"  0{arr[i, j, z]}({i},{j},{z})");
+                else Console.Write($"  {arr[i, j, z]}({i},{j},{z})");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
+int[,,] matrix = Generate3DArray(2, 2, 2);
+Dictionary<int, int> SlovarDic = GetDictionery(matrix);
+while (true)
+{
+    if (GetCheker(matrix, SlovarDic))
+    {
+        break;
+    }
+    else
+    {
+        matrix = Generate3DArray(2, 2, 2);
+        SlovarDic = GetDictionery(matrix);
+    }
+}
+
+
+Print3DArray(matrix);
+Console.WriteLine();
---
+++ Task62/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+
+
+double[,] InitMatrix()
+{
+    int rows = 4;
+    int columns = 4;
+    double[,] matrix = new double[rows, columns];
+    matrix[0, 0] = 1;
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int l = 0 + i; l < rows - rows + 1 + i; l++)
+        {
+            for (int j = 1; j < columns - i; j++)
+            {
+                matrix[l, j] = matrix[l, j - 1] + 1;
+            }
+        }
+        for (int l = 1; l < rows - i; l++)
+        {
+
+            for (int j = columns - 1 - i; j < columns - i; j++)
+            {
+                if (i > 0) matrix[l, j] = matrix[l, j - 1] + 1;
+                else matrix[l, j] = matrix[l - 1, j] + 1;
+            }
+
+        }
+        for (int l = rows - 1 - i; l > rows - 2 - i; l--)
+        {
+
+            for (int j = columns - 2; j >= 0 + i; j--)
+            {
+                if (i > 0) matrix[l, j] = matrix[l - 1, j] + 1;
+                else matrix[l, j] = matrix[l, j + 1] + 1;
+            }
+
+        }
+        for (int l = rows - 2; l >= 1 + i; l--)
+        {
+
+            for (int j = 0 + i; j < 1 + i; j++)
+            {
+                if (i > 0) matrix[l, j] = matrix[l, j + 1] + 1;
+                else
+                    matrix[l, j] = matrix[l + 1, j] + 1;
+
+            }
+
+        }
+    }
+    return matrix;
+}
+
+
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < 10) Console.Write($"0{matrix[i, j]} ");
+            else Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Console.WriteLine();
+double[,] matrix = InitMatrix();
+PrintMatrix(matrix);
+Console.WriteLine();
