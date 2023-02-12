b98002e43c1cdabd4aed5b420205f15f64f207dd
ilia <ilia100@hotmail.com>
2023-02-12 17:35:00 +0300

    Done HomeWork8

---
+++ Task54/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+
+void GetDescendingOrder(int[,] matrix)
+{
+    int[,] arr = new int[matrix.GetLength(0), matrix.GetLength(1)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+
+
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+void Print2DArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = (Generate2DArray(5, 5));
+Print2DArray(matrix);
+Console.WriteLine();
+GetDescendingOrder(matrix);
+Print2DArray(matrix);
+Console.WriteLine();
+
---
+++ Task58/Program.cs
@@ -0,0 +1,64 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+
+int [,] GetMultipMatrix(int[,] matrixA, int[,] matrixB)
+{
+    int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+    for (int f = 0; f < matrixA.GetLength(0); f++)
+    {
+        for (int i = 0; i < matrixA.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrixA.GetLength(0); j++)
+            {
+                matrixC[f,i] = matrixC[f,i]+(matrixA[f, j] * matrixB[j, i] );
+                
+            }
+        }
+
+    }
+    return matrixC;
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+void Print2DArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] matrixA = (Generate2DArray(2, 2));
+Print2DArray(matrixA);
+Console.WriteLine();
+
+int[,] matrixB = (Generate2DArray(2, 2));
+Print2DArray(matrixB);
+Console.WriteLine();
+
+Print2DArray(GetMultipMatrix(matrixA,matrixB));
+Console.WriteLine();
